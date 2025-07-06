5fff6cdb766b6389722811b321007a84847ef1c7
IFEOLUWA678 <ajibadegideon2022@gmail.com>
2025-07-06 01:32:52 +0100

    Update latest changes for Railway deployment

--- app.py
+++ app.py
@@ -1339,4 +1339,4 @@ def inject_balance():
 if __name__ == "__main__":
     init_db_pool()
     threading.Thread(target=expose_localhost).start()
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
